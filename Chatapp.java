b9d5751f4365d8ab6f5a6430ce02abb29b4b0ada
Hlogonofatso-Mphahlele <ST10519166@rcconnect.edu.za>
2026-06-11 13:24:17 +0200

    Create Chatapp.java

---
+++ Chatapp.java
@@ -0,0 +1,51 @@
+package chatapp;
+
+import java.util.Scanner;
+
+public class Chatapp {
+
+    
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        
+        System.out.println("Register Ur Account");
+        
+        System.out.print("Enter your first name: ");
+        String firstName = kb.nextLine();
+        
+        System.out.print("Enter your last name: ");
+        String lastName = kb.nextLine();
+        
+        System.out.print("Enter your username: ");
+        String username = kb.nextLine();
+        
+        System.out.print("Enter your password: ");
+        String password = kb.nextLine();
+        
+        System.out.print("Enter your cellphone number: ");
+        String cellPhoneNumber = kb.nextLine();
+        
+        Login user = new Login(username, password, cellPhoneNumber);
+        
+        String registrationResult = user.registerUser();
+        System.out.println(registrationResult);
+        
+        if (registrationResult.equals("User registered successfully")) {
+            System.out.println("Enter Details To Login");
+            
+            System.out.print("Enter your username: ");
+            String loginUsername = kb.nextLine();
+            
+            System.out.print("Enter ur password: ");
+            String loginPassword = kb.nextLine();
+            
+            boolean LoginSuccess = user.loginUser(loginUsername, loginPassword);
+            
+            System.out.println(user.returnLoginStatus(LoginSuccess, firstName, lastName));
+            
+          
+             
+        }
+     
+    }    
+    
